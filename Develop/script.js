38314e18c3406b8cae2a465944405b7d233a32a1
FuzzyGiraffee <summerflowers2357@gmail.com>
2023-02-12 08:46:38 -0600

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -15,6 +15,7 @@ function generatePassword(){
   }else if( passwordLength > 128){
     alert("Error password must be 8 to 128 characters")
   // Prompts for character types and stores as boolean
+  }
   var passwordSpecial = confirm("Include special characters?");
   var passwordLower = confirm("Include lowercase letters?");
   var passwordUpper = confirm("include uppercase letters?");
@@ -24,29 +25,39 @@ function generatePassword(){
     alert("Erorr at least one character type must be selected");
   };
   // Create arrays contiaing characters
-  var lowercase = ["a",'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+  var lowerCase = ["a",'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
    uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
    special = ["!","#","$","~"],
    num = ["1","2","3","4","5","6","7","8","9","0"];
 
-   var letterselector = random(1,26)
-   var charactertypespector = random(1,4)
-   var password = []
-   // take password lenght output -> run random that number of times ->  randomly choose be
+  //  // take password lenght output -> run random that number of times ->  randomly choose be
   //  create new variable that assigned a number to each
 
-  //begin character randomization
-  for (var i = 0; i <= passwordLength; i++ ){
-    // randomize type of character 
-    //create if else and elseif statements to select password special, password lower, passwordupper and passwordnum.
+  //create if else and elseif statements to select password special, password lower, passwordupper and passwordnum.
     //once selected use random funciton to select random character in each array and sequentiall asign to location in desired password output
-      if (charactertypespector = 1) {
-        password[i] = [letterselector]
-      }
+
+  console.log(charactertypeselector); 
+
+  //begin character randomization
+  for (var i = 0; i <= passwordLength; i++){
+    // randomize type of character 
+    characterTypeSelector = random(0,3);
+    letterSelector = random(0,25);
+      //Select character type
+      if (charactertypeselector == 0) {
+        lowercase[letterSelector]
+
+      }else if (charactertypeselector == 1){
+
+     } else if (charactertypeselector == 2){
+
+     }else {
+
      }
      console.log(password)
   }
-}
+  }
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
