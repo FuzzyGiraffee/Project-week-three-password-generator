8da4c74d32ded2ea06d434fd47b3e4a948860c58
FuzzyGiraffee <summerflowers2357@gmail.com>
2023-02-12 10:41:04 -0600

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
 // psudo-random number generator
+
+//TODO: 1) store character selection types selected during dialog box sequence 
+//      2) referece stored values in character randomization algorithm to selectivly add characters
+//      3)
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
@@ -21,42 +25,56 @@ function generatePassword(){
   var passwordUpper = confirm("include uppercase letters?");
   var passwordNum = confirm("Include numbers?");
   // Validate at lesat one character type was selected
-  if (!passwordSpecial && !passwordLower && !passwordUpper && !passweordNum) {
-    alert("Erorr at least one character type must be selected");
+  if (!passwordSpecial && !passwordLower && !passwordUpper && !passwordNum) {
+    alert("Error at least one character type must be selected");
   };
   // Create arrays contiaing characters
-  var lowerCase = ["a",'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
-   uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
-   special = ["!","#","$","~"],
-   num = ["1","2","3","4","5","6","7","8","9","0"];
+    var lowerCase = "abcdefghifjklmnopqrstuvwxyz",
+    uppercase = "ABCDEFGHIJKLMNOPQRTSTUVWXYZ",
+    special = "!#~$5^&*()",
+    num = "123457890";
 
+   //create array named password 
+   var password = [];
   //  // take password lenght output -> run random that number of times ->  randomly choose be
   //  create new variable that assigned a number to each
 
   //create if else and elseif statements to select password special, password lower, passwordupper and passwordnum.
     //once selected use random funciton to select random character in each array and sequentiall asign to location in desired password output
 
-  console.log(charactertypeselector); 
 
   //begin character randomization
-  for (var i = 0; i <= passwordLength; i++){
-    // randomize type of character 
-    characterTypeSelector = random(0,3);
-    letterSelector = random(0,25);
-      //Select character type
-      if (charactertypeselector == 0) {
-        lowercase[letterSelector]
+    for (var i = 0; i < passwordLength; i++){
+      characterTypeSelector = random(0,3);
+      letterSelector = random(0,25);
+        //Character insertion procedure
+        if (characterTypeSelector == 0) {
+          var addLetterLower = lowerCase[letterSelector]
+          console.log (addLetterLower)
+          password.push(addLetterLower);
+          console.log(password)
 
-      }else if (charactertypeselector == 1){
+        }else if (characterTypeSelector == 1){
+          var addLetterUpper = uppercase[letterSelector]
+          console.log (addLetterUpper)
+          password.push(addLetterUpper);
+          console.log(password)
 
-     } else if (charactertypeselector == 2){
-
-     }else {
-
-     }
-     console.log(password)
+        }else if (characterTypeSelector == 2){
+          var addLetterSpecial = special[letterSelector]
+          console.log (addLetterSpecial)
+          password.push(addLetterSpecial);
+          console.log(password)
+          
+        }else {
+          var addLetterNum = num[letterSelector]
+          console.log (addLetterNum)
+          password.push(addLetterNum);
+          console.log(password)
+          
+    }
   }
-  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
